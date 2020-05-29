e69d347155fa27c36b0847a67fa052adb3a58852
Johannes Battenberg <battenberg@stud.tu-darmstadt.de>
2020-05-29 14:43:55 +0200

    pypi package fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pri3o_dmenu_desktop", # Replace with your own username
-    version="0.1.1",
+    version="0.1.2",
     author="Johannes Battenberg",
     author_email="joe@chaoticneutral.eu",
     description="Drop-in replacement for i3-dmenu-desktop with simple app priorization ",
